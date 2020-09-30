567bdda18bdc7e5108d2b4c74555ee242f71f623
UGUR GUNDOGDU <ugurgundogdu@me.com>
2020-09-30 00:23:37 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,16 @@
-# Write your #display_rainbow method here
+def display_rainbow
+  red = "R"
+  orange = "O"
+  yellow = "Y"
+  green = "G"
+  blue = "B"
+  indigo = "I"
+  violet = "V"
+  puts "R is #{red}"
+  puts "O is #{orange}"
+  puts "Y is #{yellow}"
+  puts "G is #{green}"
+  puts "B is #{blue}"
+  puts "I is #{indigo}"
+  puts "V is #{violet}"
+end
