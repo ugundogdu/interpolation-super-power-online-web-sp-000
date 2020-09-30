55d003f8cb129cb32ef0cee81871c2effcd4f1ac
UGUR GUNDOGDU <ugurgundogdu@me.com>
2020-09-30 00:20:32 +0000

    Automatically backed up by Learn

--- spec/display_rainbow_spec.rb
+++ spec/display_rainbow_spec.rb
@@ -6,14 +6,4 @@ describe '#display_rainbow' do
 
 
 
-    colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
-
-    expect(colors).to receive(:[]).with(0).at_least(:once).and_return("red")
-    expect(colors).to receive(:[]).with(1).at_least(:once).and_return("orange")
-    expect(colors).to receive(:[]).with(2).at_least(:once).and_return("yellow")
-    expect(colors).to receive(:[]).with(3).at_least(:once).and_return("green")
-    expect(colors).to receive(:[]).with(4).at_least(:once).and_return("blue")
-    expect(colors).to receive(:[]).with(5).at_least(:once).and_return("indigo")
-    expect(colors).to receive(:[]).with(6).at_least(:once).and_return("violet")
-
-  
+    
